e1a79ca430a923e420353e6f921c3675ae8d2ed3
Toby Kreiman <tobykreiman@dhcp-18-189-48-74.dyn.MIT.EDU>
2017-07-12 10:07:03 -0400

    testing for polestar

---
+++ TestPolestar.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 12 10:03:58 2017
+
+@author: tobykreiman
+"""
+print("------------")
+print("Hello Polestart!")
+print("Hello Polestart!")
+print("Hello World")
+print("------------")
